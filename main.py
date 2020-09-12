23013413348dec6029909ef04111307da9969098
S.Ali Mousavi <s.alimousavi2001@gmail.com>
2020-09-12 23:04:57 +0430

    find how to extract form values

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from twill.commands import *
 import config
 
+#fill the login page
 go(config.login_page)
 fv("1", "username", config.username)
 fv("1", "password", config.password)
 submit('0')
+
+def is_checked(item):
+    value=showforms()[int(item)][0][3].value
+    print(value)
+    if value=="0":
+        return(True)
+    elif value=="1":
+        return(False)
+
+
 def find_unchecked(course):
-    go(courses["course"]) #TODO read form values
+    go(config.courses[course])
+    completionstate=[]
+    for i in range(1,len(showforms())):
+        print(is_checked(i))
+        completionstate.append(is_checked(i))
+    print(completionstate)
+
 
 def check_choosen():
     pass
