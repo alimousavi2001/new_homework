77338ca915256dd6d969904e2106ad760146c7d5
alimousavi2001 <s.alimouavi2001@gmail.com>
2020-09-14 20:08:25 +0430

    add some funcs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from twill.commands import *
+from twill.commands import go,fv,submit,showforms
 import config
 
 #fill the login page
@@ -9,24 +9,36 @@ submit('0')
 
 def is_checked(item):
     value=showforms()[int(item)][0][3].value
+    title=showforms()[int(item)][0][2].value
     print(value)
     if value=="0":
-        return(True)
+        return(title,True)
     elif value=="1":
-        return(False)
+        return(title,False)
 
-
-def find_unchecked(course):
+def find_status(course):
     go(config.courses[course])
-    completionstate=[]
+    completionstate={}
     for i in range(1,len(showforms())):
-        print(is_checked(i))
-        completionstate.append(is_checked(i))
-    print(completionstate)
+        #print(is_checked(i))
+        completionstate[is_checked(i)[0]]=is_checked(i)[1]
+    return(completionstate)
 
+def find_all_status():
+    global all_homeworks
+    all_homeworks={}
+    for course in config.courses:
+        all_homeworks[course]=find_status(course)
+    return(all_homeworks)
 
-def check_choosen():
-    pass
-def find_all_unchecked():
-    for course in courses:
-        find_unchecked()
+def warn_unchecked():
+    find_all_status()
+    unchecked={}
+    for course in all_homeworks:
+        unchecked[course]=[]
+        for subject in all_homeworks[course]:
+            if all_homeworks[course][subject]==False:
+                unchecked[course].append(subject)
+    return(unchecked)
+
+print(warn_unchecked())
